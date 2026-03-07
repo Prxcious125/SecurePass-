7d5e108a2a992adb3e65d7bb7f751bb52bd91f2d
Prxcious125 <preciousepa@gmail.com>
2026-03-07 02:27:59 +0100

    Add Exercise2.kt for list processing functions

---
+++ Exercises/Exercise2.kt
@@ -0,0 +1,20 @@
+fun processList(
+    numbers: List<Int>,
+    predicate: (Int) -> Boolean   
+): List<Int> {
+    return numbers.filter { predicate(it) }
+}
+
+fun exercise2() {
+    val nums = listOf(1, 2, 3, 4, 5, 6)
+
+    val even = processList(nums) { it % 2 == 0 }
+    println("Original list : $nums")
+    println("Even numbers  : $even")
+
+    val greaterThanThree = processList(nums) { it > 3 }
+    println("Greater than 3: $greaterThanThree")
+
+    val odd = processList(nums) { it % 2 != 0 }
+    println("Odd numbers   : $odd")
+}
